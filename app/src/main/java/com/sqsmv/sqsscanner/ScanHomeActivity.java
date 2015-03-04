ff080ce8b9604a6d744fbc11c399323572fde428
Cory <coryma86@gmail.com>
2015-03-04 12:49:20 -0500

    Made config/pair activities scrollable

--- app/src/main/java/com/sqsmv/sqsscanner/ScanHomeActivity.java
+++ app/src/main/java/com/sqsmv/sqsscanner/ScanHomeActivity.java
@@ -240,9 +240,10 @@ public class ScanHomeActivity extends Activity
     {
         boolean scannerLock = config.getBoolean("scannerLock", false);
         // unregister the scanner
+        unregisterReceiver(receiver);
+
         if(!scannerLock)
         {
-            unregisterReceiver(receiver);
 
             // indicate this view has been destroyed
             // if the reference count becomes 0 ScanAPI can
